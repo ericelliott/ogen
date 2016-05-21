ab04f11c58004cad505ee6d37924f39301314376
Eric Elliott <support@paralleldrive.com>
2016-05-20 20:30:56 -0600

    Implement basic observable.

--- source/test/index.js
+++ source/test/index.js
@@ -1,1 +1,26 @@
-throw new Error('No tests yet!');
+const test = require('tape');
+
+const ogen = require('ogen');
+
+test('basic observable', assert => {
+  const msg = 'should create observable from generator';
+
+  const generator = function* () {
+    yield 1;
+    yield 2;
+    yield 3;
+  };
+  const observable = ogen(generator)();
+
+  const actual = [];
+  const expected = [1, 2, 3];
+
+  observable.subscribe(item => {
+    actual.push(item);
+  },
+  null,
+  () => {
+    assert.same(actual, expected, msg);
+    assert.end();
+  });
+});
